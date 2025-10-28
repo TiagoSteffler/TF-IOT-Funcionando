9b2fa3903f05825bc85fe6915934706a967a0d27
SamSteffler <samuel.steffler@acad.ufsm.br>
2025-10-28 11:29:41 -0300

    Adicionado projeto em Vite/Vue no Docker

---
+++ AppServer/IOT-Funcionando-web/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import ESP32Interface from './components/ESP32Interface.vue'
+</script>
+
+<template>
+  <ESP32Interface />
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
+}
+
+#app {
+  width: 100%;
+  height: 100vh;
+}
+</style>
